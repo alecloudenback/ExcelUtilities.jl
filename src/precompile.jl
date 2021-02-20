432fb576d112f5f06e4ca5a323823920896f91e9
Alec Loudenback <alecloudenback@gmail.com>
2021-02-19 20:31:29 -0600

    fix precompile 2

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,7 +1,6 @@
-function _precompile_(
-        #xlclip
-        src_array = "1\t2\t3\t4\n5\t6\t7\t8\n9\t10\t11\t12"
-        d = xlclip_reader(src_array)
-        xlclip_writer(d)
-    
-)
+function _precompile_()
+    #xlclip
+    src_array = "1\t2\t3\t4\n5\t6\t7\t8\n9\t10\t11\t12"
+    d = xlclip_reader(src_array)
+    xlclip_writer(d)
+end
